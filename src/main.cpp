ba5e5b42344f91e3fbe10ccc22f2162c61508833
Danilo Torchio <danilotorchio@gmail.com>
2023-02-13 12:38:00 -0300

    Configurações de bluetooth.

--- src/main.cpp
+++ src/main.cpp
@@ -4,60 +4,114 @@
 #include <HTTPClient.h>
 #include <Arduino_JSON.h>
 
-#include "string.h"
-#include "BluetoothSerial.h"
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
 
+#include "string.h"
 #include "config.h"
 
 #define LORA_BAND 915E6 // 915 MHz
 
+#define BLE_SERVER_NAME "pH Station"
+#define BLE_SERVICE_UUID "65426fae-f917-4c56-9026-2720358d212a"
+#define BLE_CHARACTERISTIC_UUID "0951f60d-8f74-4612-9b85-d38cb6b36395"
+
+BLEServer *pServer = NULL;
+BLECharacteristic *pCharacteristc = NULL;
+
 // Control variables
-BluetoothSerial SerialBT;
 Station_Config config;
 HTTPClient http;
 
-String bt_cmd;
-String bt_cmd_value;
+String ble_cmd;
+String ble_cmd_value;
 String lora_ph_value;
+String lora_temp_value;
+
 bool has_data_to_upload = false;
+bool ble_device_connected = false;
+bool ble_restart_advertising = false;
 
 // Functions declarations
+void init_peripherals();
 void setup_storage();
-void setup_peripherals();
+void setup_ble();
 void setup_wifi();
 void setup_rtc();
 void setup_lora();
-
-void bt_data_cb(const uint8_t *buff, size_t size);
 
 // Tasks declarations
 void task_bt_execute_cmd(void *params);
 void task_lora_receive_data(void *params);
 void task_wifi_send_data(void *params);
 void task_wifi_scan_net(void *params);
+void task_turn_display_onoff(void *params);
+
+class StationBLEServerCallbacks : public BLEServerCallbacks
+{
+    void onConnect(BLEServer *pServer)
+    {
+        ble_device_connected = true;
+        Serial.println("BLE: Device connected");
+    }
+    void onDisconnect(BLEServer *pServer)
+    {
+        ble_device_connected = false;
+        ble_restart_advertising = true;
+        Serial.println("BLE: Device disconnected");
+    }
+};
+
+class StationBLECharacteristicCallbacks : public BLECharacteristicCallbacks
+{
+    void onWrite(BLECharacteristic *pCharacteristc)
+    {
+        std::string value = pCharacteristc->getValue();
+
+        String str = String(value.c_str());
+        str.trim();
+
+        if (!str.isEmpty())
+        {
+            Serial.printf("BLE: Command received=%s\n", str.c_str());
+            ble_cmd = str.substring(0, 2);
+
+            if (str.length() > 2)
+                ble_cmd_value = str.substring(3);
+        }
+    }
+};
 
 void setup()
 {
     Heltec.begin(true, true, true, true, LORA_BAND);
     delay(100);
 
-    SerialBT.onData(bt_data_cb);
-    SerialBT.begin("pH_Station");
-
     Heltec.display->setFont(ArialMT_Plain_10);
     Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
 
-    setup_storage();
-    setup_peripherals();
+    // Setups
+    init_peripherals();
 
     // Tasks
     xTaskCreatePinnedToCore(task_bt_execute_cmd, "task_bt_execute_cmd", 2048, NULL, 3, NULL, 1);
     xTaskCreate(task_lora_receive_data, "task_lora_receive_data", 2048, NULL, 2, NULL);
+
+    // Turn off the display after 10 seconds
+    xTaskCreate(task_turn_display_onoff, "task_turn_display_onoff", 1024, 0, 1, NULL);
 }
 
 void loop()
 {
-    // Empty! Jobs are done in tasks.
+    if (ble_restart_advertising)
+    {
+        ble_restart_advertising = false;
+        pServer->startAdvertising();
+    }
+
+    // Jobs are done in tasks.
 }
 
 void setup_storage()
@@ -75,17 +129,21 @@ void setup_storage()
     config.load();
 }
 
-void setup_peripherals()
+void init_peripherals()
 {
     Heltec.display->clear();
+    setup_storage();
 
     if (config.loaded)
     {
+        Serial.println(config.to_json_string());
+
+        setup_ble();
         setup_wifi();
         setup_rtc();
         setup_lora();
 
-        Heltec.display->drawString(0, 40, "phStation is ready!");
+        Heltec.display->drawString(0, 50, "phStation is ready!");
     }
     else
     {
@@ -97,12 +155,53 @@ void setup_peripherals()
     Heltec.display->display();
 }
 
+void setup_ble()
+{
+    Heltec.display->drawString(0, 0, "Setting BLE...     ");
+    Heltec.display->display();
+
+    BLEDevice::init(BLE_SERVER_NAME);
+
+    // BLE Server
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new StationBLEServerCallbacks());
+
+    // BLE Service
+    BLEService *pService = pServer->createService(BLE_SERVICE_UUID);
+
+    // BLE Characteristics and Descriptors
+    pCharacteristc = pService->createCharacteristic(
+        BLE_CHARACTERISTIC_UUID,
+        BLECharacteristic::PROPERTY_READ |
+            BLECharacteristic::PROPERTY_WRITE |
+            BLECharacteristic::PROPERTY_NOTIFY |
+            BLECharacteristic::PROPERTY_INDICATE);
+
+    pCharacteristc->setCallbacks(new StationBLECharacteristicCallbacks());
+    pCharacteristc->addDescriptor(new BLE2902());
+
+    // Start service
+    pService->start();
+
+    // Start advertisng
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(BLE_SERVICE_UUID);
+    pAdvertising->setScanResponse(true);
+    pAdvertising->setMinPreferred(0x0);
+
+    pServer->startAdvertising();
+    // BLEDevice::startAdvertising();
+
+    Heltec.display->drawString(90, 0, "OK");
+    Heltec.display->display();
+}
+
 void setup_wifi()
 {
     WiFi.disconnect(true);
     delay(100);
 
-    Heltec.display->drawString(0, 0, "Setting WiFi...     ");
+    Heltec.display->drawString(0, 10, "Setting WiFi...     ");
     Heltec.display->display();
 
     WiFi.mode(WIFI_MODE_STA);
@@ -119,9 +218,9 @@ void setup_wifi()
     }
 
     if (WiFi.status() == WL_CONNECTED)
-        Heltec.display->drawString(90, 0, "OK");
+        Heltec.display->drawString(90, 10, "OK");
     else
-        Heltec.display->drawString(90, 0, "Failed");
+        Heltec.display->drawString(90, 10, "Failed");
 
     Heltec.display->display();
     delay(100);
@@ -129,7 +228,7 @@ void setup_wifi()
 
 void setup_rtc()
 {
-    Heltec.display->drawString(0, 10, "Setting RTC...      ");
+    Heltec.display->drawString(0, 20, "Setting RTC...      ");
     Heltec.display->display();
 
     struct tm timeinfo;
@@ -137,7 +236,7 @@ void setup_rtc()
 
     if (!getLocalTime(&timeinfo))
     {
-        Heltec.display->drawString(90, 10, "Failed");
+        Heltec.display->drawString(90, 20, "Failed");
         Heltec.display->display();
         return;
     }
@@ -145,13 +244,13 @@ void setup_rtc()
     setenv("TZ", "<-03>3", 1);
     tzset();
 
-    Heltec.display->drawString(90, 10, "OK");
+    Heltec.display->drawString(90, 20, "OK");
     Heltec.display->display();
 }
 
 void setup_lora()
 {
-    Heltec.display->drawString(0, 20, "Setting LoRa...     ");
+    Heltec.display->drawString(0, 30, "Setting LoRa...     ");
     Heltec.display->display();
 
     LoRa.setSpreadingFactor(12);    // Fator de espalhamento
@@ -161,25 +260,8 @@ void setup_lora()
     LoRa.setSyncWord(0x12);         // Palavra de sincronização
     LoRa.crc();
 
-    Heltec.display->drawString(90, 20, "OK");
+    Heltec.display->drawString(90, 30, "OK");
     Heltec.display->display();
-}
-
-void bt_data_cb(const uint8_t *buff, size_t size)
-{
-    if (buff != null && size > 0)
-    {
-        String str(buff, size);
-        str.trim();
-
-        if (!str.isEmpty())
-        {
-            bt_cmd = str.substring(0, 2);
-
-            if (str.length() > 2)
-                bt_cmd_value = str.substring(3);
-        }
-    }
 }
 
 // Tasks implementations
@@ -190,45 +272,50 @@ void task_bt_execute_cmd(void *params)
 
     while (1)
     {
-        if (!bt_cmd.isEmpty())
+        if (ble_device_connected && !ble_cmd.isEmpty())
         {
-            // Respond to device to inform that the request was received
-            SerialBT.println("OK");
+            pCharacteristc->setValue("OK");
+            pCharacteristc->notify();
+            delay(10);
 
             // 01: Update station configuration
-            if (bt_cmd == "01")
+            if (ble_cmd == "01")
             {
-                if (!bt_cmd_value.isEmpty())
+                if (!ble_cmd_value.isEmpty())
                 {
-                    esp_err_t ret = config.from_json_string(bt_cmd_value.c_str());
+                    esp_err_t ret = config.from_json_string(ble_cmd_value.c_str());
 
                     if (ret == ESP_OK)
                     {
                         if ((ret = config.save()) == ESP_OK)
                         {
                             config.loaded = true;
-                            setup_peripherals();
+                            // restart wifi configuration
                         }
                     }
                 }
             }
             // 02: Scan WiFi networks
-            else if (bt_cmd == "02")
+            else if (ble_cmd == "02")
             {
                 xTaskCreate(task_wifi_scan_net, "task_wifi_scan_net", 4096, NULL, 2, NULL);
             }
             // 08: Clear station configuration
-            if (bt_cmd == "08")
+            if (ble_cmd == "08")
             {
                 esp_err_t ret = config.clear();
 
                 if (ret == ESP_OK)
-                    setup_peripherals();
+                {
+                    // restart wifi configuration
+                }
             }
-            else if (bt_cmd == "09")
+            else if (ble_cmd == "09")
             {
                 Serial.println(config.to_json_string().c_str());
-                SerialBT.println(config.to_json_string().c_str());
+
+                pCharacteristc->setValue(config.to_json_string().c_str());
+                pCharacteristc->notify();
             }
 
             clear = true;
@@ -236,13 +323,13 @@ void task_bt_execute_cmd(void *params)
 
         if (clear)
         {
-            bt_cmd.clear();
-            bt_cmd_value.clear();
+            ble_cmd.clear();
+            ble_cmd_value.clear();
 
             clear = false;
         }
 
-        vTaskDelay(SerialBT.hasClient() ? 400 : 10000 / portTICK_PERIOD_MS);
+        vTaskDelay(ble_device_connected ? 400 : 10000 / portTICK_PERIOD_MS);
     }
 }
 
@@ -266,8 +353,13 @@ void task_lora_receive_data(void *params)
             while (has_data_to_upload)
                 vTaskDelay(2000 / portTICK_PERIOD_MS);
 
+            String strPacket = String(packet);
+
             lora_ph_value.clear();
-            lora_ph_value = packet;
+            lora_ph_value = strPacket.substring(0, strPacket.indexOf(";"));
+
+            lora_temp_value.clear();
+            lora_temp_value = strPacket.substring(strPacket.indexOf(";") + 1);
 
             has_data_to_upload = true;
             xTaskCreate(task_wifi_send_data, "task_wifi_send_data", 2048, NULL, 1, NULL);
@@ -286,7 +378,8 @@ void task_wifi_send_data(void *params)
             time_t tt = time(NULL);
 
             char body[60];
-            sprintf(body, "{\"reading\":%s,\"timestamps\":%d}", lora_ph_value.c_str(), int32_t(tt));
+            sprintf(body, "{\"reading\":%s,\"temp\":%s,\"timestamps\":%d}", lora_ph_value.c_str(), lora_temp_value.c_str(), int32_t(tt));
+            Serial.println(body);
 
             http.begin(config.api_url.c_str());
 
@@ -309,12 +402,15 @@ void task_wifi_send_data(void *params)
 
 void task_wifi_scan_net(void *params)
 {
-    if (SerialBT.hasClient())
+    if (ble_device_connected)
     {
         int n = WiFi.scanNetworks();
 
         if (n == 0)
-            SerialBT.println("No networks found");
+        {
+            pCharacteristc->setValue("[]");
+            pCharacteristc->notify();
+        }
         else
         {
             JSONVar jsonArray;
@@ -368,9 +464,25 @@ void task_wifi_scan_net(void *params)
             json_string.trim();
             json_string.replace(" ", "%20");
 
-            SerialBT.println(json_string.c_str());
+            pCharacteristc->setValue(json_string.c_str());
+            pCharacteristc->notify();
         }
     }
+
+    vTaskDelete(NULL);
+}
+
+void task_turn_display_onoff(void *params)
+{
+    int param = (int)params;
+
+    if (param == 0)
+    {
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
+        Heltec.display->displayOff();
+    }
+    else
+        Heltec.display->displayOn();
 
     vTaskDelete(NULL);
 }
